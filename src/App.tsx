81719778aa29f571883f190aa37b407848612a06
Marcus Mosiah Galbraith <marcusg4byu@gmail.com>
2024-03-07 13:35:19 -0700

    final product

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import './App.css';
+import teamCards from './CollegeBasketballTeams.json';
 
-const TeamCards = ['Card 1', 'Card 2', 'Card 3'];
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
 
 function HeadingIntro() {
-  return <h1>Welcome to March Madness!</h1>;
+  return (
+    <div>
+      <h1>Welcome to March Madness!</h1>
+      <p>
+        This website is simple. It is simply a list of all the college football
+        teams, along with its school mascot and location (city, state
+        abbreviation). Each school is spaced out for your reading convenience.
+        Keep in mind that there is not specific order of how the schools are
+        listed. If you have any questions, feel free to call at 601-601-6001.
+        Happy scrolling!
+      </p>
+    </div>
+  );
+}
+
+function Team(props: TeamProps) {
+  const { school, name, city, state } = props;
+
+  return (
+    <div>
+      <h2>School: {school}</h2>
+      <h3>Mascot: {name}</h3>
+      <h3>Location: {city + ', ' + state}</h3>
+      <br />
+    </div>
+  );
 }
 
 function TeamList() {
   return (
     <div>
-      <h2>#1: {TeamCards[0]}</h2>
-      <h2>#2: {TeamCards[1]}</h2>
-      <h2>#3: {TeamCards[2]}</h2>
+      {teamCards.teams.map((team, index) => (
+        <Team key={index} {...team} />
+      ))}
     </div>
   );
 }
